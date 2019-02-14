1e41e40f7846f43ab62b1646063703f3dc9a7ef4
Bunsin NANH <bunsinnanh22@gmail.com>
2019-02-15 01:20:26 +0700

    edit layout file

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -12,66 +12,121 @@
         width:17.2%;
     }
 </style>
- <script src="{{asset('js/app.js')}}"></script>
- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('css/app.css')}}">
-<nav class="navbar navbar-expand-sm ">
-    <!-- Brand -->
-    <a class="navbar-brand" href="#">
-        <img src="img/brand-logo.png" alt="brand-logo" style="width:86%;">
-    </a>
-        <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-                {{-- <div class="row"> --}}
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                    <i class="material-icons text-white">settings</i>
-                    {{-- <span>Settings</span> --}}
+    <script src="{{asset('js/app.js')}}"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+<div class="wrapper">
+    <!-- Sidebar  -->
+    <nav id="sidebar" class="bg-primary">
+        <div class="sidebar-header bg-primary">
+            <h3 class="center-text pr-4"><img src="img/brand-logo.png" alt="brand-logo" style="width:100%;"></h3>
+            <strong><img src="img/icons.png" alt="icon" style="width:90%;"></strong>
+        </div>
+        <ul class="list-unstyled components">
+            <li class="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle bg-primary">
+                    <i class="fas fa-home"></i>
+                    Home
                 </a>
-                {{-- </div> --}}
-                <div class="dropdown-menu">
-                    <p class="text-center">
-                        <a class="dropdown-item" href="#">
-                            <i class="material-icons ">account_circle</i>
-                            <span>Profile</span>
-                        </a>
-                        <a class="dropdown-item" href="#">
-                            <i class="material-icons">exit_to_app</i>
-                            <span>Logout</span>
-                        </a>
-                    </p>
-                </div>
-            </li>              
+                <ul class="collapse list-unstyled bg-secondary" id="homeSubmenu">
+                    <li>
+                        <a href="#">Home 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-briefcase"></i>
+                    About
+                </a>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <i class="fas fa-copy"></i>
+                    Pages
+                </a>
+                <ul class="collapse list-unstyled bg-secondary" id="pageSubmenu">
+                    <li>
+                        <a href="#">Page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-image"></i>
+                    Portfolio
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-question"></i>
+                    FAQ
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fas fa-paper-plane"></i>
+                    Contact
+                </a>
+            </li>
         </ul>
-</nav>
 
-<nav class="site-nav pt-3">
-    <ul>
-        <li>
-            <div class="row">
-                <a class="home-post-link" href="/circulardesign.html"><i class="material-icons">account_balance</i>&nbsp;</a>
-                <span> Job Post</span>
+        <ul class="list-unstyled CTAs bg-primary">
+            <li>
+                <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+            </li>
+            <li>
+                <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Page Content  -->
+    <div id="content">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+            <div class="container-fluid">
+
+                <button type="button" id="sidebarCollapse" class="btn btn-primary">
+                    <i class="fas fa-align-left"></i>
+                    <span>Menu</span>
+                </button>
+                <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-align-justify"></i>
+                </button>
+
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul class="nav navbar-nav ml-auto ">
+                        <li class="nav-item active">
+                            <a class="nav-link text-white" href="#"><i class="fas fa-sign-out-alt"> </i> &nbsp; Logout</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#"><i class="fas fa-cog"></i>&nbsp; Settings</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </li>
-        <li>
-            <div class="row">
-                <a class="home-post-link" href="/circulardesign.html"><i class="material-icons">list</i>&nbsp;</a>
-                <span> ERO Management</span>
-            </div>
-        </li>
-        <li>
-            <div class="row">
-                <a class="home-post-link" href="/circulardesign.html"><i class="material-icons">school</i>&nbsp;</a>
-                <span> Students Table</span>
-            </div>
-        </li>
-        <li>
-            <div class="row">
-                <a class="home-post-link" href="/circulardesign.html"><i class="material-icons">palette</i>&nbsp;</a>
-                <span> Occupancies</span>
-            </div>
-        </li>
-    </ul>
-</nav>
+        </nav>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $('#sidebarCollapse').on('click', function () {
+                    $('#sidebar').toggleClass('active');
+                });
+            });
+        </script>
 
 @section('content')
     
--- resources/views/pages/students.blade.php
+++ resources/views/pages/students.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.layout')
-
+@yield('content')
 @section('content')
 <div class="container">
     <div class="row">
